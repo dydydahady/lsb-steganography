4c263273c8677cb495bf8b6d3e7b0869b01d7b7e
annawinn <81415022+annawinn@users.noreply.github.com>
2021-10-09 22:22:15 +0800

    Add files via upload

---
+++ 2004/audio_stega.py
@@ -0,0 +1,50 @@
+import wave
+
+def case(a):
+	if a == 1:
+		encode()
+	elif a == 2:
+		decode()
+	elif a == 3:
+		quit()
+	else:
+		print("\nEnter valid Choice!")
+
+def encode():
+	inputwav = input("Enter wav file name (include .wav extension): ")
+	audio = wave.open(inputwav,mode="rb")
+	frame_bytes = bytearray(list(audio.readframes(audio.getnframes())))
+	outputwav = input("Enter output wav file name (include .wav extension): ")
+	string = input("Enter message to encode: ")
+	print("\nEncoding...")
+	string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
+	bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
+	for i, bit in enumerate(bits):
+	    frame_bytes[i] = (frame_bytes[i] & 254) | bit
+	frame_modified = bytes(frame_bytes)
+	#for i in range(0,10):
+	#	print(frame_bytes[i])
+	newAudio =  wave.open(outputwav, 'wb')
+	newAudio.setparams(audio.getparams())
+	newAudio.writeframes(frame_modified)
+
+	newAudio.close()
+	audio.close()
+	encodedmsg = string.split("###")[0]
+	print(encodedmsg, "successfully encoded into", outputwav)
+
+def decode():
+	secretwav = input("Enter encoded wav file name (include .wav extension): ")
+	print("\nDecoding...")
+	audio = wave.open(secretwav, mode='rb')
+	frame_bytes = bytearray(list(audio.readframes(audio.getnframes())))
+	extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+	string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+	decoded = string.split("###")[0]
+	print("Sucessfully decoded message: "+decoded)
+	audio.close()	
+
+while(1):
+	print("\nSelect an option: \n1)Encode\n2)Decode\n3)exit")
+	val = int(input("\nChoice:"))
+	case(val)
